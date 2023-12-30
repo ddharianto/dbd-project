011ff779ee6da7a30c9c3b5aff8e82aad9ea174c
Dede Harianto <ddharianto11@gmail.com>
2023-12-31 03:23:55 +0700

    Update codes

--- train/preprocessing.py
+++ train/preprocessing.py
@@ -8,48 +8,89 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 
 nltk.download('stopwords')
 nltk.download('punkt')
-stop = stopwords.words('indonesian')
+nltk_stopwords = set(stopwords.words('indonesian'))
+words_excluded = {'hari', 'satu', 'dua', 'tiga', 'empat', 'lima', 'enam', 'tujuh', 'delapan', 'sembilan'}
+stop = nltk_stopwords - words_excluded
 label_encoder = preprocessing.LabelEncoder()
 bow = CountVectorizer()
 tf_idf = TfidfVectorizer()
 
-df = pd.read_excel('./dataset/Dataset DBD.xlsx', sheet_name=3)
-df.astype('str')
+print()
 
-print(df['Kesadaran'].unique())
+df_textual = pd.read_excel('./dataset/Dataset DBD.xlsx', sheet_name=3)
+# df_textual.astype('str')
 
-df['Kesadaran'] = df['Kesadaran'].replace([' Compos Mentis'], 'Compos Mentis') # Menyamakan value yang tidak sesuai
-print(df['Kesadaran'].unique())
+print(df_textual)
 
-df['Kesadaran'] = label_encoder.fit_transform(df['Kesadaran']) #Mengubah value menjadi angka
-print(df['Kesadaran'].unique())
+df_textual = df_textual.drop(['Diagnosis Akhir', 'Tingkat DBD'], axis=1)
+print(df_textual)
+print(df_textual.dtypes)
 
-print(df['Imunisasi'].unique())
+print(df_textual['Kesadaran'].unique())
 
-print(df['Imunisasi'].isnull().sum())
+df_textual['Kesadaran'] = df_textual['Kesadaran'].replace([' Compos Mentis'], 'Compos Mentis') # Menyamakan value yang tidak sesuai
+print(df_textual['Kesadaran'].unique())
 
-df['Imunisasi'].fillna(df['Imunisasi'].mode()[0], inplace=True) # mengisi value NaN dengan nilai mean
-print(df['Imunisasi'].unique())
+df_textual['Kesadaran'] = label_encoder.fit_transform(df_textual['Kesadaran']) # Mengubah value menjadi angka
+print(df_textual['Kesadaran'].unique())
 
-df['Imunisasi'] = label_encoder.fit_transform(df['Imunisasi']) #Mengubah value menjadi angka
-print(df['Imunisasi'].unique())
+print(df_textual['Imunisasi'].unique())
+print(df_textual['Imunisasi'].isnull().sum())
 
-print(df[df.columns[1:12]])
+df_textual['Imunisasi'].fillna(df_textual['Imunisasi'].mode()[0], inplace=True) # Mengisi value NaN dengan nilai modus
+print(df_textual['Imunisasi'].unique())
 
-df[df.columns[1:12]] = df[df.columns[1:12]].apply(label_encoder.fit_transform) #Mengubah value menjadi angka
-print(df[df.columns[1:12]])
+df_textual['Imunisasi'] = label_encoder.fit_transform(df_textual['Imunisasi']) # Mengubah value menjadi angka
+print(df_textual['Imunisasi'].unique())
 
-print(df[['Keluhan Utama', 'Riwayat Penyakit Sekarang']])
+print(df_textual[df_textual.columns[1:11]])
 
-df[['Keluhan Utama', 'Riwayat Penyakit Sekarang']] = df[['Keluhan Utama', 'Riwayat Penyakit Sekarang']].apply(lambda x: x.astype(str).str.lower()) # mengubah menjadi lowercase
-df[['Keluhan Utama', 'Riwayat Penyakit Sekarang']] = df[['Keluhan Utama', 'Riwayat Penyakit Sekarang']].replace('[^a-zA-Z\s]+', ' ', regex=True) # menghilangkan simbol-simbol
-df['Keluhan Utama'] = df['Keluhan Utama'].apply(lambda x: ' '.join([word for word in x.split() if word not in (stop)])) # menghilangkan stop words
-df['Riwayat Penyakit Sekarang'] = df['Riwayat Penyakit Sekarang'].apply(lambda x: ' '.join([word for word in x.split() if word not in (stop)])) # menghilangkan stop words
-print(df[['Keluhan Utama', 'Riwayat Penyakit Sekarang']])
+df_textual[df_textual.columns[1:11]] = df_textual[df_textual.columns[1:11]].apply(label_encoder.fit_transform) # Mengubah value menjadi angka
+print(df_textual[df_textual.columns[1:11]])
+print(df_textual[df_textual.columns[1:11]].nunique())
 
-print(df.dtypes)
+print(df_textual['Keluhan Utama'])
 
-df_textual = df
+df_textual['Keluhan Utama'] = df_textual['Keluhan Utama'].astype(str).str.lower() # mengubah menjadi lowercase
+df_textual['Keluhan Utama'] = df_textual['Keluhan Utama'].replace('[^a-zA-Z0-9\s]+', ' ', regex=True) # menghilangkan simbol-simbol dan angka
+df_textual['Keluhan Utama'] = df_textual['Keluhan Utama'].apply(lambda x: ' '.join([word for word in x.split() if word not in (stop)])) # menghilangkan stop words
+# df_textual['Riwayat Penyakit Sekarang'] = df_textual['Riwayat Penyakit Sekarang'].apply(lambda x: ' '.join([word for word in x.split() if word not in (stop)])) # menghilangkan stop words
+print(df_textual['Keluhan Utama'])
+
+# Term Frequency–Inverse Document Frequency (TF-IDF)
+keluhan_utama = tf_idf.fit_transform(df_textual['Keluhan Utama'])
+keluhan_utama = keluhan_utama.toarray()
+df_textual['Keluhan Utama'] = keluhan_utama.tolist()
+
+print(df_textual['Keluhan Utama'])
+
+list_lengths = len(df_textual['Keluhan Utama'].loc[0])
+
+# Extract TF-IDF features into new columns
+max_features = list_lengths  # Replace with the maximum length of your TF-IDF vectors
+for i in range(max_features):
+    df_textual[f'KU{i+1}'] = 0.0  # Create new columns
+
+# Populate the new columns with TF-IDFvalues
+for index, row in df_textual.iterrows():
+    tfidf_values = row['Keluhan Utama']  # Extract TF-IDF vector
+    for i, value in enumerate(tfidf_values[:max_features]):  # Iterate through vector
+        df_textual.at[index, f'KU{i+1}'] = value  # Assign value to respective column
+
+# Drop the original 'Keluhan Utama' column as it's no longer needed
+df_textual.drop(columns=['Keluhan Utama'], inplace=True)
+
+print(df_textual['Diagnosa Masuk'].unique())
+
+df_textual['Diagnosa Masuk'].fillna(df_textual['Diagnosa Masuk'].mode()[0], inplace=True) # Mengisi value NaN dengan nilai modus
+print(df_textual['Diagnosa Masuk'].unique())
+
+df_textual['Diagnosa Masuk'] = label_encoder.fit_transform(df_textual['Diagnosa Masuk']) # Mengubah value menjadi angka
+print(df_textual['Diagnosa Masuk'].unique())
+
+print(df_textual)
+print(df_textual.dtypes)
+
 
 df_numeric = pd.read_excel("./dataset/Dataset DBD.xlsx", sheet_name=2)
 print(df_numeric.dtypes)
@@ -70,50 +111,45 @@ print(df_numeric.isnull().sum())
 
 print(df_numeric)
 
-df_fixed = pd.concat([df_textual, df_numeric], axis=1, join="inner") # menggabungkan kedua dataframes
-print(df_fixed)
+df_final = pd.concat([df_textual, df_numeric], axis=1, join="inner") # menggabungkan kedua dataframes
 
-print(df_fixed.isnull().sum())
-
-# df_fixed.to_csv('df_final.csv', index=0) # menyimpan dataset ke csv
-
-df_final = df_fixed
-
-# Bag of Words (BoW)
+print(df_final)
 print(df_final.dtypes)
 
-X1 = bow.fit_transform(df_final['Keluhan Utama'])
-X1 = X1.toarray()
+# # Bag of Words (BoW)
 
-Y1 = bow.fit_transform(df_final['Riwayat Penyakit Sekarang'])
-Y1 = Y1.toarray()
+# X1 = bow.fit_transform(df_final['Keluhan Utama'])
+# X1 = X1.toarray()
 
-# Term Frequency–Inverse Document Frequency (TF-IDF)
-X2 = tf_idf.fit_transform(df_final['Keluhan Utama'])
-X2 = X2.toarray()
+# Y1 = bow.fit_transform(df_final['Riwayat Penyakit Sekarang'])
+# Y1 = Y1.toarray()
 
-Y2 = tf_idf.fit_transform(df_final['Riwayat Penyakit Sekarang'])
-Y2 = Y2.toarray()
+# # Term Frequency–Inverse Document Frequency (TF-IDF)
+# X2 = tf_idf.fit_transform(df_final['Keluhan Utama'])
+# X2 = X2.toarray()
 
-df_final['Keluhan Utama'] = X2.tolist()
-df_final['Riwayat Penyakit Sekarang'] = Y2.tolist()
+# Y2 = tf_idf.fit_transform(df_final['Riwayat Penyakit Sekarang'])
+# Y2 = Y2.toarray()
 
-# df_final['Keluhan Utama'] = df_final['Keluhan Utama'].astype(str)
-# df_final['Riwayat Penyakit Sekarang'] = df_final['Riwayat Penyakit Sekarang'].astype(str)
+# df_final['Keluhan Utama'] = X2.tolist()
+# df_final['Riwayat Penyakit Sekarang'] = Y2.tolist()
 
-# def convert_to_array(x):
-#     if isinstance(x, list) or isinstance(x, np.ndarray):
-#         return np.array(x)
-#     return x
+# # df_final['Keluhan Utama'] = df_final['Keluhan Utama'].astype(str)
+# # df_final['Riwayat Penyakit Sekarang'] = df_final['Riwayat Penyakit Sekarang'].astype(str)
 
-# df_final['Keluhan Utama'] = df_final['Keluhan Utama'].apply(convert_to_array)
-# df_final['Riwayat Penyakit Sekarang'] = df_final['Riwayat Penyakit Sekarang'].apply(convert_to_array)
+# # def convert_to_array(x):
+# #     if isinstance(x, list) or isinstance(x, np.ndarray):
+# #         return np.array(x)
+# #     return x
 
-print(df_final['Keluhan Utama'].loc[[0]].values)
-print(df_final['Keluhan Utama'].values)
-print(X2)
-print(df_final.dtypes)
-print(df_final[['Keluhan Utama', 'Riwayat Penyakit Sekarang']])
+# # df_final['Keluhan Utama'] = df_final['Keluhan Utama'].apply(convert_to_array)
+# # df_final['Riwayat Penyakit Sekarang'] = df_final['Riwayat Penyakit Sekarang'].apply(convert_to_array)
+
+# print(df_final['Keluhan Utama'].loc[[0]].values)
+# print(df_final['Keluhan Utama'].values)
+# print(X2)
+# print(df_final.dtypes)
+# print(df_final[['Keluhan Utama', 'Riwayat Penyakit Sekarang']])
 
 
 df_final.to_csv('./dataset/df_final.csv', index=0) # menyimpan dataset ke csv
--- train/train2.py
+++ train/train2.py
@@ -1,79 +1,16 @@
-# from elmmodel import elm
 import numpy as np
 import pandas as pd
-# from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelBinarizer
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
 # Load dataset
-df_dbd = pd.read_csv('./dataset/new_df_final.csv')
+df_dbd = pd.read_csv('./dataset/df_final.csv')
 
 # print(df_dbd)
 
-# df_dbd["Class"] = np.nan
-# for x in range(100):
-#   if x > 79 :
-#     df_dbd.loc[x, 'Class'] = int(0)
-#   else:
-#     df_dbd.loc[x, 'Class'] = int(1)
-
-# # print(df_dbd['Class'].isnull().sum())
-# df_dbd['Class'] = df_dbd['Class'].astype(int)
-# df_dbd = df_dbd.drop(['Diagnosa Masuk', 'Diagnosis Keluar'], axis=1)
-# df_dbd.to_csv('./dataset/new_df_final.csv', index=False)
-
-# print(df_dbd)
-
-df_dbd['Keluhan Utama'] = df_dbd['Keluhan Utama'].apply(eval)
-df_dbd['Riwayat Penyakit Sekarang'] = df_dbd['Riwayat Penyakit Sekarang'].apply(eval)
-
-df_dbd['Keluhan Utama'] = np.array(df_dbd['Keluhan Utama'])
-df_dbd['Riwayat Penyakit Sekarang'] = np.array(df_dbd['Riwayat Penyakit Sekarang'])
-
-df_dbd['Keluhan Utama'] = df_dbd['Keluhan Utama'].apply(lambda x: [float(i) for i in x])
-df_dbd['Riwayat Penyakit Sekarang'] = df_dbd['Riwayat Penyakit Sekarang'].apply(lambda x: [float(i) for i in x])
-
-list_lengths = df_dbd['Keluhan Utama'].apply(len)
-
-print(len(df_dbd['Keluhan Utama'].loc[0]))
-
-# Extract TF-IDF features into new columns
-max_features = 100  # Replace with the maximum length of your TF-IDF vectors
-for i in range(max_features):
-    df_dbd[f'KU{i+1}'] = 0.0  # Create new columns
-
-# Populate the new columns with TF-IDFvalues
-for index, row in df_dbd.iterrows():
-    tfidf_values = row['Keluhan Utama']  # Extract TF-IDF vector
-    for i, value in enumerate(tfidf_values[:max_features]):  # Iterate through vector
-        df_dbd.at[index, f'KU{i+1}'] = value  # Assign value to respective column
-
-# Drop the original 'Keluhan Utama' column as it's no longer needed
-df_dbd.drop(columns=['Keluhan Utama'], inplace=True)
-
-# print(df_dbd['Keluhan Utama'].apply(type).unique())
-# print(df_dbd['Keluhan Utama'].values)
-# # print(df_dbd['Riwayat Penyakit Sekarang'].loc[[0]].dtypes)
-# print(df_dbd[['Keluhan Utama', 'Riwayat Penyakit Sekarang']])
-
-# first_row_list = df_dbd.loc[4, 'Riwayat Penyakit Sekarang']
-
-# # Check the data types of elements in the list
-# for element in first_row_list:
-#     print(type(element))
-
-y = df_dbd[["Class"]].values
-X = df_dbd.drop(['Class', 'Riwayat Penyakit Sekarang'], axis=1).values
-# X = df_dbd.drop(['Class', 'Keluhan Utama', 'Riwayat Penyakit Sekarang'], axis=1).values
-
-# Convert lists in X to numerical arrays
-X_numeric = np.array([np.array(xi) if isinstance(xi, list) else xi for xi in X])
-
-# Check for non-numeric elements in X_numeric
-for xi in X_numeric:
-    if not all(isinstance(el, (int, float)) for el in xi):
-        print("Non-numeric element detected:", xi)
+y = df_dbd[['Diagnosis DBD']].values
+X = df_dbd.drop(['Diagnosis DBD'], axis=1).values
 
 # Binarize labels
 lb = LabelBinarizer()
@@ -83,7 +20,7 @@ y = lb.fit_transform(y)
 # print("X sample:", X[0])  # Print a sample row of X
 
 # Split dataset into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X_numeric, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Model Parameters
 input_size = X.shape[1]  # Number of features
